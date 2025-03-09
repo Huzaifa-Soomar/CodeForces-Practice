6ab6d15f3ce5c28330bbc2297db9bce91d5d7c84
Huzaifa-Soomar <huzaifasoomar78@gmail.com>
2025-03-09 08:46:03 +0500

    police

--- Beautiful_Year.cpp
+++ Beautiful_Year.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int main () {
+    int y ;
+    cin >> y;
+    while(true){
+        y++;
+        int a = y/1000;    //1st digit
+        int b = (y/100)%10;// 2nd digit
+        int c = (y/10)%10;// 3rd digit
+        int d = y % 10;// 4th digit
+        if(a != b && a != c && a != d && b != c && b != d && c != d){
+            cout << y;
+            break;
+        }
+    }
+}
--- Stones_Table.cpp
+++ Stones_Table.cpp
@@ -3,15 +3,12 @@ using namespace std;
 
 int main(){
     int n , count  = 0;
-    cin >> n;
-    for(int i = 0  ; i <  n ; i++){
-        string color;
-        cin >> color;
-        if(color[i]!=color[i]){
-            count ++;
-        }else{
-            count  = 0;
-        }
+    string s;
+    cin >> n >> s;
+    for(int i = 0  ; i <  n -1 ; i++){
+       if(s[i] == s[i+1]){
+        count++;
+       }
     }
     cout << count;
 
